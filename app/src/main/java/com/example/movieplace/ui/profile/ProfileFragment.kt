c38c2d0fbfc104f543b81be0c283450f9c0c801e
anbaogasso <anbaogasso@github.com>
2021-11-10 18:14:49 +0100

    update change password

--- app/src/main/java/com/example/movieplace/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/movieplace/ui/profile/ProfileFragment.kt
@@ -1,26 +1,40 @@
 package com.example.movieplace.ui.profile
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.movieplace.ChangePassword
 import com.example.movieplace.R
+import com.example.movieplace.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     /*companion object {
         fun newInstance() = ProfileFragment()
     }*/
-
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
     private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        //return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        val button = root.findViewById<ImageButton>(R.id.button_change_password)
+        button.setOnClickListener() {
+            val intent = Intent(context, ChangePassword::class.java)
+            context?.startActivity(intent)
+        }
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
